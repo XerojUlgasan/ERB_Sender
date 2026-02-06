66b2b7cb6f474fe3a35d7bf90072cb16d74803e6
Xeroj Nilles Ulgasan <xerojulgasan@gmail.com>
2026-02-07 00:48:37 +0800

    added ternary operators for /wifiStatus

--- src/webServerHandler.h
+++ src/webServerHandler.h
@@ -117,9 +117,9 @@ bool initializeWebServer(bool deviceIsSender, Preferences& pref) {
       [](AsyncWebServerRequest *request){
         String jsonString;
         JsonDocument doc;
-        doc["ssid"] = WiFi.SSID();
+        doc["ssid"] = (WiFi.status() == WL_CONNECTED) ? WiFi.SSID() : "Not Connected";
         doc["isConnected"] = (WiFi.status() == WL_CONNECTED) ? true : false;
-        doc["ip"] = WiFi.localIP().toString();
+        doc["ip"] = (WiFi.status() == WL_CONNECTED) ? WiFi.localIP().toString() : "192.168.4.1";
 
         serializeJson(doc, jsonString);
 
